85da5f466b8301b8600aef750e0c174a23f112d8
Sahil Katkamwar <sahilkatkamwar24@gmail.com>
2025-05-09 07:14:12 +0530

    Updated Readme and made scrapper more easy

--- Assignment28_WebScrapper_Flipkart/scrapper.py
+++ Assignment28_WebScrapper_Flipkart/scrapper.py
@@ -14,9 +14,9 @@ with open("flipkart_reviews.txt", "w", encoding="utf-8") as f:
         rating = review.select_one(".XQDdHH.Ga3i8K")
         comment = review.select_one(".ZmyHeo div div")
 
-        f.write(f"Name: {name.get_text(strip=True) if name else 'N/A'}\n")
-        f.write(f"Rating: {rating.get_text(strip=True) if rating else 'N/A'}\n")
-        f.write(f"Comment: {comment.get_text(strip=True) if comment else 'N/A'}\n")
+        f.write(f"Name: {name.get_text(strip=True)}\n")
+        f.write(f"Rating: {rating.get_text(strip=True)}\n")
+        f.write(f"Comment: {comment.get_text(strip=True)}\n")
         f.write("-" * 40 + "\n")
 
 print("Saved to flipkart_reviews.txt")
--- README.md
+++ README.md
@@ -4,9 +4,10 @@
 
 - **Always perform a `git pull origin main`** to keep your assignments updated with the latest changes.
 - **Replace all dataset file paths** in the code with the correct **relative paths** based on your local directory structure.
+- **[📁 Datasets link](https://drive.google.com/drive/folders/1xAwNOPaazvYb4qqy9AY2HPugOvfT_E35?usp=sharing)**
 
 ---
 
 **✨ Be a legend. Drop a star. It's free real estate.**
 
-*✅ All the best!*
+_✅ All the best!_
